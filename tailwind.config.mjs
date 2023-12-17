ed5dab4bbcd3df2c3c314d8d5876098169733b44
Joaquín Soto <105025603+jokkun343@users.noreply.github.com>
2023-12-16 22:13:32 -0300

    Add paleta de colores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,21 @@ export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			'primary': {
+				'50': '#edfefd',
+				'100': '#d1fcfc',
+				'200': '#aaf7f7',
+				'300': '#6feff1',
+				'400': '#2ddde3',
+				'500': '#10b9c1',
+				'600': '#119ba9',
+				'700': '#157c89',
+				'800': '#1a6470',
+				'900': '#1a535f',
+				'950': '#0b3741',
+			}
+		},
 	},
 	plugins: [],
 }
