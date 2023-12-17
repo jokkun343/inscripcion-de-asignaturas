2946ff92e145286e9a84976ca4d71ba5162565d3
Joaquín Soto <105025603+jokkun343@users.noreply.github.com>
2023-12-16 23:36:25 -0300

    Fix tailwind extends colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,22 +3,23 @@ export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			'primary': {
-				'50': '#f1f9fe',
-				'100': '#e1f1fd',
-				'200': '#bde3fa',
-				'300': '#83cef6',
-				'400': '#39b2ef',
-				'500': '#189bdf',
-				'600': '#0b7cbe',
-				'700': '#0a639a',
-				'800': '#0d547f',
-				'900': '#10466a',
-				'950': '#0b2c46',
-			},
+		extend: {
+			colors:{
+				'primary': {
+					'50': '#f1f9fe',
+					'100': '#e1f1fd',
+					'200': '#bde3fa',
+					'300': '#83cef6',
+					'400': '#39b2ef',
+					'500': '#189bdf',
+					'600': '#0b7cbe',
+					'700': '#0a639a',
+					'800': '#0d547f',
+					'900': '#10466a',
+					'950': '#0b2c46',
+				},
+			}
 		},
-		extend: {},
 	},
 	plugins: [],
 }
