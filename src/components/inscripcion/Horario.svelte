bd695b69f1e8770d043ade434d40f808c7d958b0
Joaquín Soto <105025603+jokkun343@users.noreply.github.com>
2023-12-25 14:32:24 -0300

    Refactor: Componente Horario reducido a solo mostrar el horario

--- src/components/inscripcion/Horario.svelte
+++ src/components/inscripcion/Horario.svelte
@@ -1,11 +1,9 @@
 <script>
-    import data from '../../../public/data.json'
+    import { listaAsignaturas } from './asignaturasSeleccionadas.js'
 
-    let asignaturas = data.asignaturas;
+    $: asignaturasSeleccionadas = $listaAsignaturas;
 
-    const diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes","Sabado"];
-
-    let seleccionadas = [];
+    const diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sabado"];
 
     const horario = [
         "08:00",
@@ -18,50 +16,7 @@
         "18:30",
         "20:00",
     ];
-
-    function toggleOpcion(index, opcionIndex) {
-        const seleccion = {
-            nombre: asignaturas[index].nombre,
-            dia: asignaturas[index].opciones[opcionIndex].dia,
-            hora: asignaturas[index].opciones[opcionIndex].hora,
-            profesor: asignaturas[index].profesor,
-            sala: asignaturas[index].sala
-        };
-
-        seleccionadas = seleccionadas.filter(
-            (opcion) => !(opcion.nombre === seleccion.nombre),
-        );
-
-        seleccionadas = [...seleccionadas, seleccion];
-    }
 </script>
-
-<h1>Horario de Clases</h1>
-
-<div>
-    <h2>Menú de Asignaturas</h2>
-    {#each asignaturas as asignatura, index}
-        <div>
-            <p>
-                {asignatura.nombre}
-            </p>
-            <div>
-                {#each asignatura.opciones as opcion, opcionIndex}
-                    <label>
-                        <input
-                            type="radio"
-                            name={`asignatura_${index}`}
-                            bind:group={asignatura[index]}
-                            value={opcion}
-                            on:change={() => toggleOpcion(index, opcionIndex)}
-                        />
-                        {opcion.dia} a las {opcion.hora}
-                    </label>
-                {/each}
-            </div>
-        </div>
-    {/each}
-</div>
 
 <div>
     <h2>Horario de Clases</h2>
@@ -80,7 +35,7 @@
                     <td>{hora}</td>
                     {#each diasSemana as dia}
                         <td>
-                            {#each seleccionadas as asignatura}
+                            {#each asignaturasSeleccionadas as asignatura}
                                 {#if asignatura.dia === dia && asignatura.hora === hora}
                                 <div class=" text-center w-auto overflow-hidden rounded-2xl border-2 border-solid border-amber-300 bg-white p-3 shadow">
                                     <p class="text-base font-bold text-gray-800 my-0">{asignatura.nombre}</p>
