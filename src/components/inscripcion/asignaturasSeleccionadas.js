edefea5197b6d93b109ee73d2e1546787db04158
Joaquín Soto <105025603+jokkun343@users.noreply.github.com>
2023-12-25 14:30:39 -0300

    Refactor: Agregada función set en store de asignaturas

--- src/components/inscripcion/asignaturasSeleccionadas.js
+++ src/components/inscripcion/asignaturasSeleccionadas.js
@@ -1,3 +1,7 @@
 import { writable } from 'svelte/store';
 
-export const seleccionadas = writable([]);
+export const listaAsignaturas = writable([]);
+
+export const setListaAsignaturas = (nuevaLista) => {
+    listaAsignaturas.set(nuevaLista);
+}
