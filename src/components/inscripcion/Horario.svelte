aac72f8cf3db7da7c5dcc2a8ae0da40cd55cd06e
Joaquín Soto <105025603+jokkun343@users.noreply.github.com>
2023-12-25 18:47:31 -0300

    Style: Clases de Tailwind agregadas a horario

--- src/components/inscripcion/Horario.svelte
+++ src/components/inscripcion/Horario.svelte
@@ -1,75 +1,58 @@
 <script>
-    import { listaAsignaturas } from './asignaturasSeleccionadas.js'
+	import { listaAsignaturas } from './asignaturasSeleccionadas.js'
 
-    $: asignaturasSeleccionadas = $listaAsignaturas;
+	$: asignaturasSeleccionadas = $listaAsignaturas
 
-    const diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sabado"];
+	const diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado']
 
-    const horario = [
-        "08:00",
-        "09:30",
-        "11:00",
-        "12:30",
-        "14:00",
-        "15:30",
-        "17:00",
-        "18:30",
-        "20:00",
-    ];
+	const horario = ['08:00', '09:30', '11:00', '12:30', '14:00', '15:30', '17:00', '18:30', '20:00']
 </script>
 
-<div>
-    <h2>Horario de Clases</h2>
-    <table>
-        <thead>
-            <tr>
-                <th class="hora">Hora</th>
-                {#each diasSemana as dia}
-                    <th>{dia}</th>
-                {/each}
-            </tr>
-        </thead>
-        <tbody>
-            {#each horario as hora}
-                <tr>
-                    <td>{hora}</td>
-                    {#each diasSemana as dia}
-                        <td>
-                            {#each asignaturasSeleccionadas as asignatura}
-                                {#if asignatura.dia === dia && asignatura.hora === hora}
-                                <div class=" text-center w-auto overflow-hidden rounded-2xl border-2 border-solid border-amber-300 bg-white p-3 shadow">
-                                    <p class="text-base font-bold text-gray-800 my-0">{asignatura.nombre}</p>
-                                    <p class="text-xs font-light text-gray-500 my-0">Profesor {asignatura.profesor}</p>
-                                    <p class="text-xs font-light text-gray-500 my-0">Sala {asignatura.sala}</p>
-                                  </div>
-                                {/if}
-                            {/each}
-                        </td>
-                    {/each}
-                </tr>
-            {/each}
-        </tbody>
-    </table>
+<div class="box-border w-full overflow-auto">
+	<table class="box-border table h-full overflow-auto rounded-lg bg-white shadow">
+		<thead>
+			<tr>
+				<th class="hora">Hora</th>
+				{#each diasSemana as dia}
+					<th class="w-2/12 text-center font-bold text-gray-900">{dia}</th>
+				{/each}
+			</tr>
+		</thead>
+		<tbody>
+			{#each horario as hora}
+				<tr>
+					<td class="h-1/10">{hora}</td>
+					{#each diasSemana as dia}
+						<td>
+							{#each asignaturasSeleccionadas as asignatura}
+								{#if asignatura.dia === dia && asignatura.hora === hora}
+									<div
+										class=" h-full w-auto overflow-hidden rounded-2xl border-2 border-solid border-primary-600 bg-white p-3 text-center shadow">
+										<p class="my-0 text-base font-bold text-primary-800">{asignatura.nombre}</p>
+										<p class="my-0 text-sm text-gray-500">
+											Profesor {asignatura.profesor}
+										</p>
+										<p class="my-0 text-sm text-gray-500">Sala {asignatura.sala}</p>
+									</div>
+								{/if}
+							{/each}
+						</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </div>
 
 <style>
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
+	th,
+	td {
+		border: 1px solid #ddd;
+		height: 10%;
+		padding: 8px;
+	}
 
-    th,
-    td {
-        border: 1px solid #ddd;
-        padding: 8px;
-        text-align: left;
-    }
-
-    th {
-        width: 10%;
-    }
-
-    .hora {
-        width: 2%;
-    }
+	.hora {
+		width: 2%;
+	}
 </style>
