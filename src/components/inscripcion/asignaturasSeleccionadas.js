b9672eeb2f3b4114d00e93ef1f14adde423993b1
Joaquín Soto <105025603+jokkun343@users.noreply.github.com>
2023-12-25 19:21:53 -0300

    Feat: Guardado de lista en localStorage

--- src/components/inscripcion/asignaturasSeleccionadas.js
+++ src/components/inscripcion/asignaturasSeleccionadas.js
@@ -1,7 +1,15 @@
 import { writable } from 'svelte/store';
 
-export const listaAsignaturas = writable([]);
+const isLocalStorageAvailable = typeof localStorage !== 'undefined';
+
+const listaAsignaturasGuardada = isLocalStorageAvailable ? localStorage.getItem('listaAsignaturas') : null;
+const listaAsignaturasInicial = listaAsignaturasGuardada ? JSON.parse(listaAsignaturasGuardada) : [];
+
+export const listaAsignaturas = writable(listaAsignaturasInicial);
 
 export const setListaAsignaturas = (nuevaLista) => {
     listaAsignaturas.set(nuevaLista);
+    if (isLocalStorageAvailable) {
+        localStorage.setItem('listaAsignaturas', JSON.stringify(nuevaLista));
+    }
 }
