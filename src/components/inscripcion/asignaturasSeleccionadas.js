36e768b9add426e9341b91fb05582d4e4e6bc91b
Joaquín Soto <105025603+jokkun343@users.noreply.github.com>
2023-12-26 19:03:11 -0300

    Fix: Error de caché en lista de asignaturas

--- src/components/inscripcion/asignaturasSeleccionadas.js
+++ src/components/inscripcion/asignaturasSeleccionadas.js
@@ -1,15 +1,7 @@
 import { writable } from 'svelte/store';
 
-const isLocalStorageAvailable = typeof localStorage !== 'undefined';
-
-const listaAsignaturasGuardada = isLocalStorageAvailable ? localStorage.getItem('listaAsignaturas') : null;
-const listaAsignaturasInicial = listaAsignaturasGuardada ? JSON.parse(listaAsignaturasGuardada) : [];
-
-export const listaAsignaturas = writable(listaAsignaturasInicial);
+export const listaAsignaturas = writable([]);
 
 export const setListaAsignaturas = (nuevaLista) => {
     listaAsignaturas.set(nuevaLista);
-    if (isLocalStorageAvailable) {
-        localStorage.setItem('listaAsignaturas', JSON.stringify(nuevaLista));
-    }
 }
