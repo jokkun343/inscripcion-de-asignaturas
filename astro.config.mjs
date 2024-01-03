67328ddc54c404c279f26177f4351ab3b39c16ba
Joaquín Soto <105025603+jokkun343@users.noreply.github.com>
2024-01-03 17:13:18 -0300

    Fix login and new rut validation function

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   integrations: [svelte(), tailwind(), solidJs()]
 });
