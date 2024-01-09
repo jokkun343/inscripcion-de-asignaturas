78b068fdeee99a57f8de07bd53bba5b09e3b9702
Joaquín Soto <105025603+jokkun343@users.noreply.github.com>
2024-01-08 22:51:12 -0300

    Feat election list on the courses store

--- src/components/inscripcion/coursesStore.js
+++ src/components/inscripcion/coursesStore.js
@@ -1,5 +1,11 @@
 import { writable } from 'svelte/store';
 
+export const listaAsignaturasElegidas = writable([])
+
+export const setListaAsignaturasElegidas = (nuevaLista) => {
+    listaAsignaturasElegidas.set(nuevaLista);
+}
+
 export const listaAsignaturas = writable([]);
 
 export const setListaAsignaturas = (nuevaLista) => {
